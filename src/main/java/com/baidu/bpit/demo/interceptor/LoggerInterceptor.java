b7a8f9d5110c79576eb0223afb933e346bd65fd9
garychenqin <1175340090@qq.com>
2016-11-19 10:03:56 +0800

    修改细节

--- src/main/java/com/baidu/bpit/demo/interceptor/LoggerInterceptor.java
+++ src/main/java/com/baidu/bpit/demo/interceptor/LoggerInterceptor.java
@@ -1,37 +1,52 @@
 package com.baidu.bpit.demo.interceptor;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by chenshouqin on 2016-11-19 08:43.
  */
 @Component
 @Aspect
+@Order(1)
 public class LoggerInterceptor {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     ThreadLocal<Long> startTime = new ThreadLocal<Long>();
 
     @Pointcut("execution(public * com.baidu.bpit.demo.controller..*.*(..))")
-    public void webLogPointcut(){}
+    public void webLogPointcut() {
+    }
 
     @Before("webLogPointcut()")
-    public void before() {
+    public void before(JoinPoint joinPoint) {
         long beginTime = System.currentTimeMillis();
-        logger.info("start time : {}", beginTime);
         startTime.set(beginTime);
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        logger.info("URL : {}", request.getRequestURL().toString());
+        logger.info("REQUEST-METHOD : {}", request.getMethod());
+        logger.info("IP : {}", request.getRemoteAddr());
+        logger.info("METHOD : {}", joinPoint.getSignature().getDeclaringTypeName() +
+                "." + joinPoint.getSignature().getName());
     }
 
     @AfterReturning(returning = "ret", pointcut = "webLogPointcut()")
     public void after(Object ret) {
         long endTime = System.currentTimeMillis();
-        logger.info("end time : {}", endTime);
         logger.info("execute time : {}", endTime - startTime.get());
     }
 }
